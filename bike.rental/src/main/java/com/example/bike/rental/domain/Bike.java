2186cb6bfe59f5a9b17c9a9c30caba2e0b8c8dc9
mezeibertalan95 <bertalan.mezei95@gmail.com>
2020-08-04 13:28:09 +0200

    bike class added, db connection configured, somehow data.sql gets imported twice

---
+++ bike.rental/src/main/java/com/example/bike/rental/domain/Bike.java
@@ -0,0 +1,56 @@
+package com.example.bike.rental.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "BIKE")
+@NoArgsConstructor
+public @Data class Bike {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE )
+	private Long id; //primary key
+	
+	@NotNull
+	@Column(name = "manufacture_date")
+	private int manufactureDate;
+	
+	
+	@NotNull
+	@Column(name = "bike_type")
+	private String bikeType;
+	
+	@NotNull
+	@Column(name = "daily_price")
+	private int dailyPrice;
+	
+	@NotNull
+	@Column(name = "enabled")
+	private boolean enabled;
+	
+	/*@OneToMany(mappedBy="bike")
+	private List<Object> rentals;*/
+
+	public Bike(@NotNull int manufactureDate, @NotNull String bikeType, @NotNull int dailyPrice,
+			@NotNull boolean enabled) {
+		super();
+		this.manufactureDate = manufactureDate;
+		this.bikeType = bikeType;
+		this.dailyPrice = dailyPrice;
+		this.enabled = enabled;
+	}
+	
+	
+}
---
+++ bike.rental/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into bike values (nextval ('hibernate_sequence'), 'Puch Mistral', 4000, true, 1987);
+insert into bike values (nextval ('hibernate_sequence'), 'Puch Clubman', 3000, true, 1980);
